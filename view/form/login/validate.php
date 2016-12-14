b63909529faac7519ce94bf2905a028fba1282cc
camiloruizvidal <camiloruizvidal@gmail.com>
2016-12-13 22:05:43 -0500

    #007

--- view/form/login/validate.php
+++ view/form/login/validate.php
@@ -1,10 +1,10 @@
 <?php
 @session_start();
+
 if (isset($_GET['cerrarsesion']))
 {
     $_SESSION = array();
     session_destroy();
-    var_dump($_SESSION);
     exit();
 }
 $url = './login';
--- view/plantillas/reportes.php
+++ view/plantillas/reportes.php
@@ -22,36 +22,9 @@ $Validar->UsuarioCorrecto(array('Administrador'));
         <#--active--#>
     </head>
     <body>
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li id="id_peticiones"><a href="nueva_peticion">Reportes</a></li>
-                        
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown">                  
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php
-                                @session_start();
-                                echo strtoupper($_SESSION['nombre']) . ' ' . strtoupper($_SESSION['apellido']);
-                                ?> <i class="glyphicon glyphicon-user"></i><span class="caret"></span></a>                            
-                            <ul class="dropdown-menu">
-                                <li><a href="#"><i class="glyphicon glyphicon-cog"></i> configurar</a></li>
-                                <li><a href="logout"><i class="glyphicon glyphicon-off"></i> Cerrar sesion</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?php
+        include_once '../../plantillas/menu/admin.php';
+        ?>
         <div class="container-fluid" id="contenido">
             <#--contenido--#>
         </div> 
